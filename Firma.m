6028d08558dc11826a86fbbc68ae452b0dedfb27
julianisima10 <julianisima10@gmail.com>
2016-09-27 10:28:18 +0200

    Subida inicial
    Cliente Afirma iOS 1.4.8

---
+++ Firma.m
@@ -0,0 +1,14 @@
+//
+//  Firma.m
+//  SignSample02
+//
+//  Created by Juliana Marulanada on 7/1/16.
+//  Copyright © 2016 Atos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Firma.h"
+
+@implementation Firma
+
+@end
---
+++ SignSample02/AOAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AOAppDelegate.h
+//  SignSample02
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AOAppDelegate : UIResponder <UIApplicationDelegate>
+    
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
