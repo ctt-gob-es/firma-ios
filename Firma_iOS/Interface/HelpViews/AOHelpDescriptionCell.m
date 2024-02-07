9694de3b4136af076fb45bcf026b79949a0ccd11
Tania Alvarez Diaz <tania.alvarez@abamobile.com>
2024-02-07 15:22:50 +0100

    #1724 Eliminados ficheros innecesarios

--- Firma_iOS/Interface/HelpViews/AOHelpDescriptionCell.m
+++
@@ -1,9 +0,0 @@
-//
-//  AOHelpDescriptionCell.m
-//  Cliente @firma
-//
-//  Created by ABAMobile on 7/2/24.
-//  Copyright © 2024 Solid GEAR. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
