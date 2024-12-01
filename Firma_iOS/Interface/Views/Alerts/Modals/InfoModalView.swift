ca62efe97706b42bdda0c26605097182a183140a
Luis Nicieza <luis.nicieza@abamobile.com>
2024-12-01 15:34:21 +0100

    Button display improvements

--- Firma_iOS/Interface/Views/Alerts/Modals/InfoModalView.swift
+++ Firma_iOS/Interface/Views/Alerts/Modals/InfoModalView.swift
@@ -11,65 +11,68 @@ struct InfoModalView: View {
     @Environment(\.presentationMode) var presentationMode
     
     var body: some View {
-        ScrollView {
-            VStack(spacing: 20) {
-                HStack {
-                    AccessibleText(content: NSLocalizedString("info_view_title", bundle: Bundle.main, comment: ""))
-                        .mediumBoldStyle(foregroundColor: ColorConstants.Text.primary)
-                        .accessibilityAddTraits(.isHeader)
-                    Spacer()
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        ExitButton()
-                    }
-                }
-                
-                VStack(alignment: .leading, spacing: 10) {
-                    AccessibleText(content: NSLocalizedString("info_view_description", bundle: Bundle.main, comment: ""))
-                        .regularStyle(foregroundColor: ColorConstants.Text.secondary)
+        VStack(spacing: 20) {
+            ScrollView {
+                VStack(spacing: 0) {
                     
-                    AccessibleText(content: NSLocalizedString("info_view_certificate_title", bundle: Bundle.main, comment: ""))
-                        .mediumBoldStyle(foregroundColor: ColorConstants.Text.primary)
-                    
-                    HStack(alignment: .top) {
-                        IconView(iconName: "button_share")
-                        AccessibleText(content: NSLocalizedString("info_view_certificate_description_one", bundle: Bundle.main, comment: ""))
-                            .regularStyle(foregroundColor: ColorConstants.Text.primary)
-                    }
-                    
-                    HStack(alignment: .top) {
-                        IconView(iconName: "button_document")
-                        AccessibleText(content: NSLocalizedString("info_view_certificate_description_two", bundle: Bundle.main, comment: ""))
-                            .regularStyle(foregroundColor: ColorConstants.Text.primary)
-                    }
-                    
-                    Spacer()
-                    
-                    /*Button(action: {
-                        if let url = URL(string: "https://www.fondoseuropeos.hacienda.gob.es/sitios/dgpmrr/es-es/Paginas/Inicio.aspx") {
-                            UIApplication.shared.open(url)
+                    HStack {
+                        AccessibleText(content: NSLocalizedString("info_view_title", bundle: Bundle.main, comment: ""))
+                            .mediumBoldStyle(foregroundColor: ColorConstants.Text.primary)
+                            .accessibilityAddTraits(.isHeader)
+                        Spacer()
+                        Button(action: {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }) {
+                            ExitButton()
                         }
-                    }) {
-                        AccessibleText(content: NSLocalizedString("info_view_more_info", bundle: Bundle.main, comment: ""))
-                            .regularBoldStyle(foregroundColor: ColorConstants.Background.buttonEnabled)
-                            .foregroundColor(ColorConstants.Background.buttonEnabled)
-                            .underline()
-                            .frame(maxWidth: .infinity, alignment: .center)
-                    }*/
-                    
-                    Button(action: {
-                        self.presentationMode.wrappedValue.dismiss()
-                    }) {
-                        AccessibleText(content: NSLocalizedString("info_view_button_title", bundle: Bundle.main, comment: ""))
                     }
-                    .buttonStyle(CustomButtonStyle(isEnabled: true))
+                    
+                    VStack(alignment: .leading, spacing: 10) {
+                        AccessibleText(content: NSLocalizedString("info_view_description", bundle: Bundle.main, comment: ""))
+                            .regularStyle(foregroundColor: ColorConstants.Text.secondary)
+                        
+                        AccessibleText(content: NSLocalizedString("info_view_certificate_title", bundle: Bundle.main, comment: ""))
+                            .mediumBoldStyle(foregroundColor: ColorConstants.Text.primary)
+                        
+                        HStack(alignment: .top) {
+                            IconView(iconName: "button_share")
+                            AccessibleText(content: NSLocalizedString("info_view_certificate_description_one", bundle: Bundle.main, comment: ""))
+                                .regularStyle(foregroundColor: ColorConstants.Text.primary)
+                        }
+                        
+                        HStack(alignment: .top) {
+                            IconView(iconName: "button_document")
+                            AccessibleText(content: NSLocalizedString("info_view_certificate_description_two", bundle: Bundle.main, comment: ""))
+                                .regularStyle(foregroundColor: ColorConstants.Text.primary)
+                        }
+                    }
                 }
             }
-            .padding()
-            .background(Color.white)
-            .cornerRadius(10)
+                
+            Spacer()
+            
+            /*Button(action: {
+                if let url = URL(string: "https://www.fondoseuropeos.hacienda.gob.es/sitios/dgpmrr/es-es/Paginas/Inicio.aspx") {
+                    UIApplication.shared.open(url)
+                }
+            }) {
+                AccessibleText(content: NSLocalizedString("info_view_more_info", bundle: Bundle.main, comment: ""))
+                    .regularBoldStyle(foregroundColor: ColorConstants.Background.buttonEnabled)
+                    .foregroundColor(ColorConstants.Background.buttonEnabled)
+                    .underline()
+                    .frame(maxWidth: .infinity, alignment: .center)
+            }*/
+            
+            Button(action: {
+                self.presentationMode.wrappedValue.dismiss()
+            }) {
+                AccessibleText(content: NSLocalizedString("info_view_button_title", bundle: Bundle.main, comment: ""))
+            }
+            .buttonStyle(CustomButtonStyle(isEnabled: true))
         }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
     }
 }
 
