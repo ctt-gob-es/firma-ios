f80849c17be019369c3ba10ccebd2fad68b00a91
Alvaro <>
2025-10-29 09:50:42 +0100

    Accessibility Settings: Improve DNIResponsiveImageView

--- Firma_iOS/Interface/Views/DNI/DNIResponsiveImageView.swift
+++ Firma_iOS/Interface/Views/DNI/DNIResponsiveImageView.swift
@@ -6,29 +6,46 @@
 //  Copyright © 2025 Solid GEAR. All rights reserved.
 //
 
-
 import SwiftUI
 
 struct DNIResponsiveImageView: View {
     let imageName: String
+    let imageNameLandscape: String?
     let landscapeMaxHeightRatio: CGFloat
-
+    
     @Environment(\.verticalSizeClass) private var verticalSizeClass
 
-    init(imageName: String, landscapeMaxHeightRatio: CGFloat = 0.3) {
-        self.imageName = imageName
-        self.landscapeMaxHeightRatio = landscapeMaxHeightRatio
+    init(imageName: String, imageNameLandscape: String? = nil, landscapeMaxHeightRatio: CGFloat = 0.3) {
+	   self.imageName = imageName
+	   self.imageNameLandscape = imageNameLandscape
+	   self.landscapeMaxHeightRatio = landscapeMaxHeightRatio
+    }
+
+    private var currentImageName: String? {
+	   if let landscape = imageNameLandscape,
+		 verticalSizeClass == .compact {
+		  return landscape
+	   } else if verticalSizeClass != .compact {
+		  return imageName
+	   } else {
+		  return nil
+	   }
     }
 
     var body: some View {
-        let isLandscape = verticalSizeClass == .compact
+	   let isLandscape = verticalSizeClass == .compact
 
-        Image(imageName)
-            .resizable()
-            .scaledToFit()
-            .frame(
-                maxWidth: .infinity,
-                maxHeight: isLandscape ? UIScreen.main.bounds.height * landscapeMaxHeightRatio : .none
-            )
+	   Group {
+		  if let currentImageName {
+			 Image(currentImageName)
+				.resizable()
+				.scaledToFit()
+				.frame(
+				    maxWidth: .infinity,
+				    maxHeight: isLandscape ? UIScreen.main.bounds.height * landscapeMaxHeightRatio : .none
+				)
+
+		  }
+	   }
     }
 }
